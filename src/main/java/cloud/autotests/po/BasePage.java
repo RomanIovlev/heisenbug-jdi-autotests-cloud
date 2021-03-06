047dbca92e10211e080cafe1c90a55480f49d825
RomanIovlev <roman.iovlev.jdi@gmail.com>
2021-03-06 21:23:10 +0100

    use smart locators

--- src/main/java/cloud/autotests/po/BasePage.java
+++ src/main/java/cloud/autotests/po/BasePage.java
@@ -2,8 +2,7 @@ package cloud.autotests.po;
 
 import com.epam.jdi.light.elements.common.Label;
 import com.epam.jdi.light.elements.composite.WebPage;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 
 public abstract class BasePage extends WebPage {
-    @UI("Header label") public Label headerLabel;
+    public Label headerLabel;
 }
--- src/main/java/cloud/autotests/po/LoginPage.java
+++ src/main/java/cloud/autotests/po/LoginPage.java
@@ -3,13 +3,9 @@ package cloud.autotests.po;
 
 import cloud.autotests.entities.User;
 import com.epam.jdi.light.elements.common.Label;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
 import io.qameta.allure.Step;
 
-import static cloud.autotests.QASite.homePage;
-
 public class LoginPage extends BasePage {
-    @UI("Authorization form")
     public Label authorizationForm;
     LoginForm loginForm;
 
